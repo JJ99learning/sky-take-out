83716ff6a80191d56a2a57018118596bf7830487
SUPER NITRO <jjwongjj0508@gmail.com>
2025-12-08 23:03:50 +0800

    Add new MessageConstant and modify SQL exception handler message

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
@@ -29,7 +30,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
         log.error("异常信息：{}", ex.getMessage());
-        return Result.error("Username exists!");
+        return Result.error("用户名 " + MessageConstant.ALREADY_EXIST);
     }
 
 }
